6dc83a88caa6dadbf800c9e89fa3309dbfc462a3
wabens <waltkbenson@gmail.com>
2019-06-26 08:48:35 -0500

    initial commit dependancies installed and basic express server created

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { listenerCount } from 'cluster';
+
+const app = express();
+
+app.get('/', (req, res)=>{
+  res.send('GraphQL is amazing');
+});
+
+app.listen(8080, () => console.log(`Running server on port localhost:8080/graphql`) );
